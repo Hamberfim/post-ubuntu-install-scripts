707a1d2c329ce1473a09f04f6448303bfcc40b60
Hamberfim <adhamlingithub@gmail.com>
2024-06-08 11:23:22 -0500

    some testing

--- installer-scripts/app-neovim.sh
+++ installer-scripts/app-neovim.sh
@@ -1,11 +1,3 @@
 #!/bin/bash
 
-sudo add-apt-repository -y ppa:neovim-ppa/stable
-sudo apt update
 sudo apt install -y neovim
-
-if [ ! -d "$HOME/.config/nvim" ]; then
-    git clone https://github.com/LazyVim/starter ~/.config/nvim
-fi
-
-cp catppuccin.lua ~/.config/nvim/lua/plugins/theme.lua
