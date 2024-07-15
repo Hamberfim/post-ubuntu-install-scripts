7333ea2e7c25c22797aad692ba74843a0cd664fa
Anthony <33159808+Hamberfim@users.noreply.github.com>
2024-07-15 10:22:13 -0500

    Create app-ms-edge.sh

---
+++ installer-scripts/app-ms-edge.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/edge stable main" > /etc/apt/sources.list.d/microsoft-edge-dev.list'
+
+sudo apt update && sudo apt install -y microsoft-edge-stable
