a855190261bc15787c400a0100bb506efad46afe
Anthony <33159808+Hamberfim@users.noreply.github.com>
2024-07-15 10:06:14 -0500

    Update app-hombrew.sh

--- installer-scripts/app-hombrew.sh
+++ installer-scripts/app-hombrew.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh | sh
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
