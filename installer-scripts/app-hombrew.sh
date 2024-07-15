5f5a16dc645ee7d350bcca3955470e744cb9d9d6
Anthony <33159808+Hamberfim@users.noreply.github.com>
2024-07-15 10:10:16 -0500

    Update app-hombrew.sh

--- installer-scripts/app-hombrew.sh
+++ installer-scripts/app-hombrew.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+(echo; echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"') >> /home/adhamlin/.bashrc
+
+eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+
+sudo apt-get install build-essential -y
+
+brew install gcc
